404bbd0518142afc2136b91cf55184c78f60c21d
Krishna Kumar <krishnassm420012@gmail.com>
2023-11-09 23:06:13 +0530

    grid question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+function Cell({filled,onClick,isDisabled}){
+  
+  return <button disabled={isDisabled} type='button' onClick={onClick} className={filled?"cell cell-activated":"cell"}/>
+
+  
+}
 function App() {
+  const[order,setOrder]=useState([]);
+  const[isDeactivating,setIsDeactivating]=useState(false)
+  const config=[
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
+  ]
+
+  const deactivateCells=()=>{
+    setIsDeactivating(true);
+
+    const timer=setInterval(()=>{
+        setOrder((originOrder)=>{
+          const newOrder=originOrder.slice();
+          newOrder.pop()
+          if(newOrder.length===0){
+              clearInterval(timer)
+              setIsDeactivating(false);
+          }
+          return newOrder
+        })
+    },300)
+  }
+  const activateCells=(index)=>{
+    const newOrder=[...order,index];
+    
+    setOrder(newOrder);
+    console.log(newOrder)
+    if(newOrder.length===config.flat(1).filter(Boolean).length){
+      deactivateCells();
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid" style={{gridTemplateColumns:`repeat(${config[0].length},1fr)`}}>
+      {config.flat(1).map((value,index)=>{
+        
+       return value? <Cell key={index} filled={order.includes(index)} onClick={()=>activateCells(index)} isDisabled={order.includes(index)||isDeactivating}/>:<span/>
+      })}
+      
     </div>
   );
 }
